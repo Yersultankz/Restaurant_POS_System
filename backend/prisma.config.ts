92d0b803d4d07dac83e22306047c4a5e425dd335
yersultan <yersultanblender@gmail.com>
2026-05-12 18:29:43 +0500

    Update: 2026-05-12 18:29:43

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "src/models/prisma/schema.prisma",
   datasource: {
-    url: "file:./database/dev.db",
+    url: env("DATABASE_URL") || "file:./database/dev.db",
   },
 });
